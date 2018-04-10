7f22d6e317b47771c928268ddd342a83749f864c
Feiyang Xue <feiyangxue@feiyang-xues-macbook-pro.local>
2018-04-10 17:01:25 -0500

    Apr10 Static Data

---
+++ DesignCodeApp/SectionCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SectionCollectionViewCell.swift
+//  DesignCodeApp
+//
+//  Created by Feiyang Xue on 4/10/18.
+//  Copyright © 2018 Meng To. All rights reserved.
+//
+
+import UIKit
+
+class SectionCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var coverImageView: UIImageView!
+    
+}
