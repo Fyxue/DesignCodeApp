145191315c2a1da52cc791f051959b30c4b29faf
feiyang xue <fyxue@uw.edu>
2018-04-12 00:11:42 -0500

    3D Animation V1

--- DesignCodeApp/ViewController.swift
+++ DesignCodeApp/ViewController.swift
@@ -46,6 +46,24 @@ class ViewController: UIViewController {
             self.playVisualEffectView.alpha = 1
         }
     }
+
+}
+
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return sections.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "sectionCell", for: indexPath) as! SectionCollectionViewCell
+        let section = sections[indexPath.row]
+        cell.titleLabel.text = section["title"]
+        cell.captionLabel.text = section["label"]
+        cell.coverImageView.image = UIImage(named: section["image"]!)
+        cell.layer.transform = animateCell(cellFrame: cell.frame)
+        return cell
+    }
 }
 
 extension ViewController: UIScrollViewDelegate{
@@ -59,20 +77,40 @@ extension ViewController: UIScrollViewDelegate{
             deviceImageView.transform = CGAffineTransform(translationX: 0, y: -offsetY/4)
             backgroundImageView.transform = CGAffineTransform(translationX: 0, y: -offsetY/5)
         }
+        
+        if let collectionView = scrollView as? UICollectionView {
+            for cell in collectionView.visibleCells as! [SectionCollectionViewCell] {
+                // Do something with the cell
+                let indexPath = collectionView.indexPath(for: cell)!
+                let attributes = collectionView.layoutAttributesForItem(at: indexPath)
+                let cellFrame = collectionView.convert((attributes?.frame)!, to: view)
+                print(cellFrame)
+                
+                let translationX = cellFrame.origin.x/5
+                cell.coverImageView.transform = CGAffineTransform(translationX: translationX, y: 0)
+            }
+        }
+    }
+    
+    func animateCell(cellFrame: CGRect) -> CATransform3D {
+        let angleFromX = Double((-cellFrame.origin.x)/10)
+        let angle = CGFloat((angleFromX * Double.pi) / 180.0)
+        var transform = CATransform3DIdentity
+        transform.m34 = -1.0/1000
+        let rotation = CATransform3DRotate(transform, angle, 0, 1, 0)
+        
+        var scaleFromX = (1000 - (cellFrame.origin.x-200))/1000
+        let scaleMax: CGFloat = 1.0
+        let scaleMin: CGFloat = 0.6
+        if scaleFromX > scaleMax{
+            scaleFromX = scaleMax
+        }
+        if scaleFromX < scaleMin{
+            scaleFromX = scaleMin
+        }
+        let scale = CATransform3DScale(transform, scaleFromX, scaleFromX, 0)
+        return CATransform3DConcat(rotation, scale)
     }
 }
 
-extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return sections.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "sectionCell", for: indexPath) as! SectionCollectionViewCell
-        let section = sections[indexPath.row]
-        cell.titleLabel.text = section["title"]
-        cell.captionLabel.text = section["label"]
-        cell.coverImageView.image = UIImage(named: section["image"]!)
-        return cell
-    }
-}
+
