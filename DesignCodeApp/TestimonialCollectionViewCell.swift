e0aecf5249b4a15aabdaa9ef4011a7f3f9852576
feiyang xue <fyxue@uw.edu>
2018-05-02 22:05:51 -0500

    May 2 - Container View

---
+++ DesignCodeApp/TestimonialCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TestimonialCollectionViewCell.swift
+//  DesignCodeApp
+//
+//  Created by XueFeiyang on 5/2/18.
+//  Copyright © 2018 Meng To. All rights reserved.
+//
+
+import UIKit
+
+class TestimonialCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    @IBOutlet weak var avatarImageView: UIImageView!
+}
